aaeef9901f41e97215aecc53de64474c2584de9f
GoliasVictor <dev.jvsb@gmail.com>
2023-04-26 17:08:36 -0300

    test

--- microjogos/2023S1/projeto-jose-victor/creditos.gd
+++ microjogos/2023S1/projeto-jose-victor/creditos.gd
@@ -7,6 +7,7 @@ var title_en = "Bat"
 # Detalhes dos créditos do seu jogo
 var credits_pt = """
 Criado por: José Victor Santana Barbosa
+TEST
 Texturas usadas do jogo Sneedy de Lazy Fox Licenciadas sobre  CC BY 4.0. 
 """
 var credits_en = """
